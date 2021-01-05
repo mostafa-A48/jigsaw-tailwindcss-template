60416114f3445f7b3186f9aa72e78ea459cfd386
Mostafa <mostafarezaie.500@gmail.com>
2021-01-05 15:33:36 +0430

    Fix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,42 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-let build = require('./tasks/build.js');
+// const mix = require('laravel-mix');
+// const tailwindcss = require('tailwindcss');
+// let build = require('./tasks/build.js');
 
+// require('laravel-mix-jigsaw');
+
+// mix.disableSuccessNotifications();
+// mix.setPublicPath('source/assets/build');
+// mix.webpackConfig({
+//   plugins: [
+//       build.jigsaw,
+//       build.browserSync(),
+//       build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
+//   ]
+// });
+
+// mix
+//   .js('source/_assets/js/main.js', 'js')
+//   .sass('source/_assets/sass/main.scss', 'css')
+//   .options({
+//     processCssUrls: false,
+//     postCss: [ tailwindcss('./tailwind.config.js') ],
+//   })
+//   .version();
+
+const mix = require('laravel-mix');
 require('laravel-mix-jigsaw');
+const tailwindcss = require('tailwindcss')
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
-mix.webpackConfig({
-  plugins: [
-      build.jigsaw,
-      build.browserSync(),
-      build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-  ]
-});
 
-mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css')
-    .options({
-      processCssUrls: false,
-      postCss: [ tailwindcss('./tailwind.config.js') ],
-    })
-    .version();
+mix.jigsaw()
+   .js('source/_assets/js/main.js', 'js')
+   .options({
+       processCssUrls: false,
+       postCss: [
+           tailwindcss('tailwind.config.js'),
+       ]
+   })
+   .sass('source/_assets/sass/main.scss', 'css')
+   .version();
